9ba182d133356fc61b51c45a838cf668b99b0678
iamshingiegitgit <shingiraideke@gmail.com>
2022-04-09 19:25:49 +0200

    All media type and size upload and download

---
+++ src/main/java/com/iamshingie/mediauploadanddownload/MediaUploadAndDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.iamshingie.mediauploadanddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaUploadAndDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaUploadAndDownloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iamshingie/mediauploadanddownload/MediaUploadAndDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamshingie.mediauploadanddownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaUploadAndDownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
